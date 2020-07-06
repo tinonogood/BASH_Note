d679b4e0cce5087ebfbb37ce3bb4548db4466365
tinonogood <tinonogood@users.noreply.github.com>
2020-07-06 11:28:40 +0800

    Update 20200706_run.sh

--- 20200706_run.sh
+++ 20200706_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# BASH_SOURCE[0] 等價於 BASH_SOURCE ,取得當前執行的 shell 檔案所在的路徑及檔名
+# dirname  去除檔名中的非目錄部分
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/bar.sh
