d9a10b53e3fb3020fa1f440b68d2436678e9a67c
tinonogood <tinonogood@users.noreply.github.com>
2020-07-27 13:03:57 +0800

    Update 20200727_dialog.sh

--- 20200727_dialog.sh
+++ 20200727_dialog.sh
@@ -25,65 +25,65 @@ function show_table_form () {
 }
 
 function add () {
-  echo "Please enter Name:"
-  read -p "Name = " name
-  echo "Please enter Phone:"
-  read -p "Phone = " phone
+echo "Please enter Name:"
+read -p "Name = " name
+echo "Please enter Phone:"
+read -p "Phone = " phone
 
-  sqlite3 $DB_FILE << EOF
-  INSERT INTO customers (Name,Phone)
-  VALUES ('$name','$phone');
-  .quit
-  EOF
+sqlite3 $DB_FILE << EOF
+INSERT INTO customers (Name,Phone)
+VALUES ('$name','$phone');
+.quit
+EOF
 }
 
 function del () {
-  echo "Please enter Id you want to delete:"
-  read -p "Id = " id
+echo "Please enter Id you want to delete:"
+read -p "Id = " id
 
-  sqlite3 $DB_FILE << EOF
-  DELETE FROM customers WHERE Id=$id;
-  .quit
-  EOF
+sqlite3 $DB_FILE << EOF
+DELETE FROM customers WHERE Id=$id;
+.quit
+EOF
 }
 
 function list_data () {
-  sqlite3 $DB_FILE << EOF>"SQLOUT"
-  .header on
-  .mode column
-  SELECT * FROM customers;
-  .quit
-  EOF
-  MSG=$(cat SQLOUT)
-  dialog --title "$OPTIONS4" --msgbox "$MSG" 10 60
-  rm -f SQLOUT
+sqlite3 $DB_FILE << EOF>"SQLOUT"
+.header on
+.mode column
+SELECT * FROM customers;
+.quit
+EOF
+MSG=$(cat SQLOUT)
+dialog --title "$OPTIONS4" --msgbox "$MSG" 10 60
+rm -f SQLOUT
 }
 
 
 function main_menu() {
-  echo "${TITLE}"
-  dialog --radiolist "$TITLE" 10 60 $O_NUMS "$TAG1" "$OPTIONS1" on "$TAG2" "$OPTIONS2" off "$TAG3" "$OPTIONS3" off "$TAG4" "$OPTIONS4" off 2>$TMP
-  ACTION=$(cat "$TMP")
-  rm -f $TMP
-  case "$ACTION" in
-      1 )
-        show_table_form
-        main_menu
-      ;;
-      2 )
-        add
-        main_menu
-      ;;
-      3 )
-        del
-        main_menu
-      ;;
-      4 )
-        list_data
-        main_menu
-      ;;
-      *) echo "Invalid option. Try another one.";;
-  esac
+echo "${TITLE}"
+dialog --radiolist "$TITLE" 10 60 $O_NUMS "$TAG1" "$OPTIONS1" on "$TAG2" "$OPTIONS2" off "$TAG3" "$OPTIONS3" off "$TAG4" "$OPTIONS4" off 2>$TMP
+ACTION=$(cat "$TMP")
+rm -f $TMP
+case "$ACTION" in
+    1 )
+      show_table_form
+      main_menu
+    ;;
+    2 )
+      add
+      main_menu
+    ;;
+    3 )
+      del
+      main_menu
+    ;;
+    4 )
+      list_data
+      main_menu
+    ;;
+    *) echo "Invalid option. Try another one.";;
+esac
 }
 
 main_menu
